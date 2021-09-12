f2a90c591d04d17fc3fd8940a5fa86b16aacea42
= <=>
2021-09-12 23:04:26 +0700

    all

---
+++ js/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    $('.review-inner').slick({
+        // autoplay: true,
+        // autoplaySpeed: 2000,
+        dots: true,
+        arrows: false,
+    });
+    $('.products-slick').slick({
+        arrows: true,
+        slidesToShow: 3,
+        
+    });
+    $('.row-responsive').slick({
+        
+        slidesToShow:4,
+        arrows: true,
+        dots: true,
+    });
+    // $('.introduce-responsive').slick({
+        
+    //     slidesToShow: 1,
+    //     arrows: false,
+    //     dots: true,
+    // });
+});
